5ac9254edfd8d79790399fdfdbec9c39b9841fb3
TheTallPaul <paul.lantow@gmail.com>
2020-07-19 21:41:15 -0700

    Added winner check
    Adding a useEffect to the Game function to check for potential winner whenever the squares hook
    updates.

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
+import { SquareFilled } from "./Game";
 import Square from "./Square";
 
 // legalSquare determines if it is legal to place a game piece at the provided
@@ -12,7 +13,7 @@ function legalSquare(
 ): boolean {
   // No existing piece at spot and is on bottom row or above a placed piece
   if (
-    squares[row][col].length === 0 &&
+    !SquareFilled(squares[row][col]) &&
     (row === squares.length - 1 || squares[row + 1][col].length > 0)
   ) {
     return true;
@@ -32,28 +33,24 @@ function Board(props: boardProps) {
   let boardSquares = [];
 
   // Build the board
-  for (let rowIndex = 0; rowIndex < props.squares.length; rowIndex++) {
+  for (let row = 0; row < props.squares.length; row++) {
     let rowSquares = [];
-    for (
-      let colIndex = 0;
-      colIndex < props.squares[rowIndex].length;
-      colIndex++
-    ) {
+    for (let col = 0; col < props.squares[row].length; col++) {
       rowSquares.push(
         <Square
           piece={
-            props.squares[rowIndex][colIndex].length > 0
-              ? props.squares[rowIndex][colIndex]
+            props.squares[row][col].length > 0
+              ? props.squares[row][col]
               : blankSquareSymbol
           }
-          onClick={() => props.onClick(rowIndex, colIndex)}
-          key={colIndex}
-          legal={legalSquare(props.squares, rowIndex, colIndex)}
+          onClick={() => props.onClick(row, col)}
+          key={col}
+          legal={legalSquare(props.squares, row, col)}
         />
       );
     }
     boardSquares.push(
-      <Grid container item spacing={0} key={rowIndex}>
+      <Grid container item spacing={0} key={row}>
         {rowSquares}
       </Grid>
     );
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Board from "./Board";
+
+export function SquareFilled(square: string): boolean {
+  return square.length > 0;
+}
+
+// winningLine finds if the provided squares match each other and are filled
+function winningLine(
+  square1: string,
+  square2: string,
+  square3: string,
+  square4: string
+): boolean {
+  if (
+    SquareFilled(square1) &&
+    square2 === square1 &&
+    square3 === square1 &&
+    square4 === square1
+  ) {
+    return true;
+  }
+
+  return false;
+}
 
 // Game keeps track of the active player and winners of the Connect Four game
 function Game() {
@@ -15,9 +38,10 @@ function Game() {
       .fill([])
       .map(() => Array<string>(numCols).fill(""))
   );
+  const [winner, setWinner] = useState("");
 
   // handleClick adds the new piece and sets the next player
-  function handleClick(row: number, col: number) {
+  const handleClick = (row: number, col: number) => {
     let squaresCopy = squares.map((row) => {
       return row.slice();
     });
@@ -25,13 +49,80 @@ function Game() {
 
     setSquares(squaresCopy);
     setDarkIsNext(!darkIsNext);
-  }
+  };
+
+  useEffect(() => {
+    // Check if there's a winner and if so, set the winner
+    for (let row = 0; row < squares.length; row++) {
+      for (let col = 0; col < squares[row].length; col++) {
+        let winnerFound = false;
+        // Horizontal line
+        if (
+          col < squares[row].length - 3 &&
+          winningLine(
+            squares[row][col],
+            squares[row][col + 1],
+            squares[row][col + 2],
+            squares[row][col + 3]
+          )
+        ) {
+          winnerFound = true;
+        }
+        // Vertical line
+        else if (
+          row < squares.length - 3 &&
+          winningLine(
+            squares[row][col],
+            squares[row + 1][col],
+            squares[row + 2][col],
+            squares[row + 3][col]
+          )
+        ) {
+          winnerFound = true;
+        }
+
+        // Downward diagonal line
+        else if (
+          row < squares.length - 3 &&
+          col < squares[row].length - 3 &&
+          winningLine(
+            squares[row][col],
+            squares[row + 1][col + 1],
+            squares[row + 2][col + 2],
+            squares[row + 3][col + 3]
+          )
+        ) {
+          winnerFound = true;
+        }
+        // Upward diagonal line
+        else if (
+          row > squares.length - 4 &&
+          col < squares[row].length - 3 &&
+          winningLine(
+            squares[row][col],
+            squares[row - 1][col + 1],
+            squares[row - 2][col + 2],
+            squares[row - 3][col + 3]
+          )
+        ) {
+          winnerFound = true;
+        }
+
+        if (winnerFound) {
+          !darkIsNext ? setWinner(darkSymbol) : setWinner(lightSymbol);
+        }
+      }
+    }
+  }, [squares, darkIsNext]);
 
   return (
-    <Board
-      squares={squares}
-      onClick={(row: number, col: number) => handleClick(row, col)}
-    />
+    <div>
+      <Board
+        squares={squares}
+        onClick={(row: number, col: number) => handleClick(row, col)}
+      />
+      {winner}
+    </div>
   );
 }
 
